e453a125e8ecad78dcc54c11d3d47dc7fcf462f1
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-03-10 13:53:55 +0530

    Update greater_element_finder.js

--- greater_element_finder.js
+++ greater_element_finder.js
@@ -9,7 +9,7 @@ function func() {
     //Original array
     let array = [2, 5, 8, 1, 4];
 
-    //CHecking for condition in array
+    //Checking for condition in array
     let value = array.some(isGreaterThan5);
 
     console.log(value);
